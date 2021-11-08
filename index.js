9edcb673cb6a7047bc079d502b9a901bde09a7cd
Jonathan <jonathantoledo11@gmail.com>
2021-11-07 22:57:27 -0800

    commit tree 9c487aa9f584443cd2868ce97ab96d496062c55a

---
+++ index.js
@@ -0,0 +1,59 @@
+// function Monday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Pump iron");
+// }
+
+// const { spy } = require("chai");
+
+// function Tuesday() {
+//     console.log("Go for a for a five-mile run");
+//     console.log("Swim 40 laps");
+// }
+
+// function Wednesday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Go for a five-mile run");
+// }
+
+// function Thursday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Pump iron");
+// }
+
+// function Friday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Swim 40 laps");
+// }
+
+
+// function runFiveMiles() {
+//     console.log("Go for a five-mile run");
+// }
+
+// function liftWeights() {
+//     console.log("Pump iron");
+// }
+
+// function swimFortyLaps() {
+//     console.log("Swim 40 laps");
+// }
+
+function Monday() {
+    exerciseRoutine(liftWeights);
+}
+
+
+// function exerciseRoutine(postRunActivity) {
+//     runFiveMiles();
+//     postRunActivity();
+// }
+
+// exerciseRoutine(() => console.log("Stretch! Work that core!"));
+
+// function receivesAFunction() {
+//     const spy = chai.spy();
+//     receivesAFunction(spy);
+// }
+// receivesAFunction()
+
+
